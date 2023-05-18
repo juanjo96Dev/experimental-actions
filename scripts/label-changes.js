8e75ddf0cd21f26c396c1aede10b0657893bad66
juanjo96dev <juanjo96developer@gmail.com>
2023-05-18 21:25:42 +0200

    🚀 chore(auto-tag.yml): update auto-tag workflow to trigger on pull request events 📦 chore(package.json): add dependencies for label-changes script ✨ feat(label-changes.js): add script to add or remove labels based on file changes in a pull request The auto-tag workflow has been updated to trigger on pull request events instead of pull_request_target. This change allows the workflow to trigger on more events, including pull request edits and reopens. The package.json file has been updated to include the dependencies required for the label-changes script. The label-changes script has been added to the scripts directory and is used to add or remove labels based on file changes in a pull request. The script uses the GitHub API to get the list of files in a pull request and then adds or removes labels based on the files that have been changed.

---
+++ scripts/label-changes.js
@@ -0,0 +1,58 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const token = core.getInput('github-token');
+    const octokit = github.getOctokit(token);
+
+    const prNumber = github.context.payload.pull_request.number;
+    const prFiles = await getPRFiles(octokit, prNumber);
+
+    const labelConfigurations = {
+      migration: '/migrations/*/*.migrations.ts',
+      test: '/test/*/*.spect.ts',
+      docs: '/docs/*'
+    };
+
+    for (const label in labelConfigurations) {
+      const pattern = labelConfigurations[label];
+      const filesMatched = prFiles.filter(file => file.filename.match(pattern));
+
+      if (filesMatched.length > 0) {
+        await addLabel(octokit, prNumber, label);
+      } else {
+        await removeLabel(octokit, prNumber, label);
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function getPRFiles(octokit, prNumber) {
+  const response = await octokit.pulls.listFiles({
+    ...github.context.repo,
+    pull_number: prNumber
+  });
+
+  return response.data;
+}
+
+async function addLabel(octokit, prNumber, label) {
+  await octokit.issues.addLabels({
+    ...github.context.repo,
+    issue_number: prNumber,
+    labels: [label]
+  });
+}
+
+async function removeLabel(octokit, prNumber, label) {
+  await octokit.issues.removeLabel({
+    ...github.context.repo,
+    issue_number: prNumber,
+    name: label
+  });
+}
+
+run();
