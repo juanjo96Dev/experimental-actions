174dacd87060236f00db45a275dd7bfff75848d2
juanjo96dev <juanjo96developer@gmail.com>
2023-05-18 21:44:29 +0200

    🟢 chore(auto-tag.yml): upgrade actions/checkout and actions/setup-node to v3 💾 chore(auto-tag.yml): add caching for node_modules ⚙️ chore(auto-tag.yml): use npm ci instead of npm install 🔎 chore(label-changes.js): use process.env.GITHUB_TOKEN instead of core.getInput('github-token') The upgrade to v3 of actions/checkout and actions/setup-node provides better performance and stability. Caching node_modules speeds up the workflow by avoiding unnecessary installations. Using npm ci instead of npm install ensures that the exact dependencies are installed, which is important for reproducibility. Using process.env.GITHUB_TOKEN instead of core.getInput('github-token') is a simpler and more secure way to access the token.

--- scripts/label-changes.js
+++ scripts/label-changes.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const token = core.getInput('github-token');
+    const token = process.env.GITHUB_TOKEN;
     const octokit = github.getOctokit(token);
 
     const prNumber = github.context.payload.pull_request.number;
